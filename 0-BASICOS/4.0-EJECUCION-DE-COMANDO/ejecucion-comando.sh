35c5d81c716645fd09118227a2a536162d329f9d
HackingSecurity <jcandela@ucm.es>
2021-10-14 12:19:26 +0200

    Programacion bash scripting

---
+++ 0-BASICOS/4.0-EJECUCION-DE-COMANDO/ejecucion-comando.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Programa para revisar como ejecutar comandos dentro de un programa y almacenar en una variable para 
+#su posterior utilización
+
+#Autor DIY
+
+ubicacionActual= pwd
+infoKernel= umane -a
+
+echo "La ubicación actual es la siguiente $ubicacionActual"
+echo "Informacion del kernel $infoKernel"
